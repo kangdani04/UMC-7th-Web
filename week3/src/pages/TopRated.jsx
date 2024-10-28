c96d3f56785255501cd51ac55d0c7a08bc59deb8
kangdani04 <ksnowflower@naver.com>
2024-10-28 10:39:52 +0900

    feat:3주차 미션 수정

--- week3/src/pages/TopRated.jsx
+++ week3/src/pages/TopRated.jsx
@@ -1,51 +1,44 @@
-// src/pages/TopRated.jsx
+// src/pages/Upcoming.jsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from '../components/Card';
+import * as S from '../styles/movies.style.js'; // 스타일 파일 경로에 맞게 수정
 
-const TopRated = () => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
+const Upcoming = () => {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchTopRatedMovies = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/movie/top-rated`, {
-            params: {
-              api_key: process.env.REACT_APP_TMDB_API_KEY,
-              language: 'ko-KR',
-            },
-          }
-        );
-        setMovies(response.data.results);
-      } catch (error) {
-        console.error("Error fetching top rated movies:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                const response = await axios.get(
+                    'https://api.themoviedb.org/3/movie/upcoming', {
+                    params: {
+                        api_key: import.meta.env.VITE_TMDB_API_KEY,
+                        language: 'ko-KR',
+                        page: 1
+                    },
+                }
+                );
+                setMovies(response.data.results);
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getMovies();
+    }, []);
 
-    fetchTopRatedMovies();
-  }, []);
+    if (loading) return <p>영화를 불러오는 중입니다...</p>;
 
-  return (
-    <div>
-      <h1 style={{ color: 'white' }}>높은 평가를 받은 영화</h1>
-      {loading ? (
-        <p>영화를 불러오는 중입니다...</p>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id} style={{ color: 'white' }}>
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-              <h2>{movie.title}</h2>
-              <p>개봉일: {movie.release_date}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    return (
+        <S.CardList>
+            {movies.map((movie) => (
+                <Card key={movie.id} movie={movie} />
+            ))}
+        </S.CardList>
+    );
 };
 
-export default TopRated;
+export default Upcoming;
